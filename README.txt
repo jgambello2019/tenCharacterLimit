2712e2aeef1c10c7f5973b9ef189d92897e17314
jgambello2019 <jgambello2019@pingry.org>
2016-03-03 22:02:44 -0500

    Second Commit. Couldn't fix the space issue. Tried anything I could think of. Instead I replaced the line under ten characters with one above ten characters.

--- README.txt
+++ README.txt
@@ -4,5 +4,3 @@ Jack Gambello
 Program goes through lines of a document and cuts any line over ten characters down to ten.
 
 I got rid of spaces between my strings in my input.txt file to make it easier to see that the strings were being cut down to ten.
-
-Not all strings were over ten characters. If this was the case they remained the same
--- tenCharacterLimit.py
+++ tenCharacterLimit.py
@@ -4,19 +4,19 @@ f.close()
 
 f = open('output.txt','w')
 output = ''
-x = 0
 
 for line in lines:
-	#while x == 0:
 	length = len(line)
 	while length > 10:
 		line = line[:-1]
 		length = len(line)
-		#if line == lines[-1]:
-			#x = 1
 	output += line
-	output +='\n'
-print(output)
+	if line != lines[-1]:
+		output += '\n'
 
+
+
+print(output)
+output.strip()
 f.write(output)
 		
