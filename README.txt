085a271ec6f277f41580c165cd2a82250d2fbe14
jgambello2019 <jgambello2019@pingry.org>
2016-03-07 23:16:21 -0500

    Was able to get rid of random spaces in the final product. Program now works exactly as I wanted it to.

--- README.txt
+++ README.txt
@@ -1,6 +1,8 @@
 Jack Gambello
-3/3
+3/7
 
 Program goes through lines of a document and cuts any line over ten characters down to ten.
 
 I got rid of spaces between my strings in my input.txt file to make it easier to see that the strings were being cut down to ten.
+
+My program will keep any string length ten or under the way it is
--- tenCharacterLimit.py
+++ tenCharacterLimit.py
@@ -1,22 +1,20 @@
-f = open('input.txt','r')
-lines = f.readlines()
+f = open('input.txt','r') #opens file
+lines = f.read().splitlines() #reads file and splits up file by line
 f.close()
 
-f = open('output.txt','w')
+f = open('output.txt','w') #Opens file to write cut down characters to
 output = ''
 
-for line in lines:
-	length = len(line)
-	while length > 10:
+for line in lines: #goes through each line
+	length = len(line) #checks if line is longer then 10 to start
+	while length > 10: #10,11 cut down string by the last character
 		line = line[:-1]
-		length = len(line)
-	output += line
-	if line != lines[-1]:
-		output += '\n'
+		length = len(line) #checks the length of the line to see if it is less than 10
+	output += line #adds the cut down string to output
+	output +='\n'
+output = output[0 :-1] #gets rid of final space at the end of output that was appearing
+print(output) #shows the user their cutdown file
 
-
-
-print(output)
-output.strip()
-f.write(output)
+f.write(output) #writes the output to the file
+f.close()
 		
